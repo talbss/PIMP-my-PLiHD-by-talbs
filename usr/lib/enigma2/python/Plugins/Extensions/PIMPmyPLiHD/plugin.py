2e67f63f208a2edb65ccce02954a90df92a631e9
talbss <b_boutaleb@hotmail.com>
2014-03-16 21:43:58 +0100

    Big changes
    - Sync with latest PLiHD official skin changes
    - change the way skin customization is done

--- usr/lib/enigma2/python/Plugins/Extensions/PIMPmyPLiHD/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/PIMPmyPLiHD/plugin.py
@@ -38,6 +38,7 @@ from skin import parseColor
 from Components.Label import Label
 import gettext
 from Tools.Directories import fileExists, resolveFilename, SCOPE_LANGUAGE, SCOPE_PLUGINS
+from shutil import move, copy
 
 #############################################################
 
@@ -50,19 +51,19 @@ config.plugins.MetrixWeather.tempUnit = ConfigSelection(default="Celsius", choic
 				("Fahrenheit", _("Fahrenheit"))
 				])
 
-config.plugins.PIMPmyPLiHD.PiconsChoice = ConfigSelection(default="infobar-shd-picon", choices = [
-				("infobar-shd-picon", _("SHD (220x132)")),
-				("infobar-picon", _("Normal (100x60)"))
+config.plugins.PIMPmyPLiHD.PiconsChoice = ConfigSelection(default="normalpicon", choices = [
+				("shdpicon", _("SHD (220x132)")),
+				("normalpicon", _("Normal (100x60)"))
 				])
 	
-config.plugins.PIMPmyPLiHD.InfobarWeatherWidget = ConfigSelection(default="no-weather", choices = [
+config.plugins.PIMPmyPLiHD.InfobarWeatherWidget = ConfigSelection(default="noweather", choices = [
 				("weather", _("Yes")),
-				("no-weather", _("No"))
+				("noweather", _("No"))
 				])
 
-config.plugins.PIMPmyPLiHD.InfobarWeatherCityWidget = ConfigSelection(default="no-city", choices = [
+config.plugins.PIMPmyPLiHD.InfobarWeatherCityWidget = ConfigSelection(default="nocity", choices = [
 				("city", _("Yes")),
-				("no-city", _("No"))
+				("nocity", _("No"))
 				])
 			
 				
@@ -121,15 +122,15 @@ class PIMPmyPLiHD(ConfigListScreen, Screen):
 	skin = """
 <screen name="PIMPmyPLiHD" position="40,40" size="1200,640" flags="wfNoBorder" backgroundColor="#40000000">
   <eLabel font="Regular; 20" foregroundColor="#00ffffff" backgroundColor="#40000000" halign="left" position="37,605" size="250,33" text="Cancel" transparent="1" />
-  <eLabel font="Regular; 20" foregroundColor="#00ffffff" backgroundColor="#40000000" halign="left" position="335,605" size="250,33" text="Save" transparent="1" />
-    <eLabel font="Regular; 20" foregroundColor="#00ffffff" backgroundColor="#40000000" halign="left" position="642,605" size="250,33" text="Reboot" transparent="1" />
+  <eLabel font="Regular; 20" foregroundColor="#00ffffff" backgroundColor="#40000000" halign="left" position="337,605" size="250,33" text="Save" transparent="1" />
+  <eLabel font="Regular; 20" foregroundColor="#00ffffff" backgroundColor="#40000000" halign="left" position="637,605" size="250,33" text="Reboot" transparent="1" />
+  <eLabel font="Regular; 20" foregroundColor="#00ffffff" backgroundColor="#40000000" halign="left" position="937,605" size="250,33" text="Information" transparent="1" />
  <widget name="config" position="21,77" scrollbarMode="showOnDemand" size="590,506" transparent="1" />
-  <eLabel position="20,15" size="348,50" text="PIMPmyPLiHD" font="Regular; 40" valign="center" transparent="1" backgroundColor="#40000000" />
-  <eLabel position="223,18" size="349,50" text="Setup" foregroundColor="#00ffffff" font="Regular; 30" valign="center" backgroundColor="#40000000" transparent="1" halign="left" />
-  <eLabel position="625,600" size="5,40" backgroundColor="#00ffff00" />
+  <eLabel position="20,15" size="348,50" text="PIMPmyPLiHD configuration panel" font="Regular; 30" valign="center" transparent="1" backgroundColor="#40000000" />
+  <eLabel position="920,600" size="5,40" backgroundColor="#000000ff" />
+  <eLabel position="620,600" size="5,40" backgroundColor="#00ffff00" />
   <eLabel position="320,600" size="5,40" backgroundColor="#0000ff00" />
   <eLabel position="20,600" size="5,40" backgroundColor="#00ff0000" />
-  #<widget name="metrixVersion" position="987,11" size="200,30" backgroundColor="#40000000" foregroundColor="#00ffffff" transparent="1" halign="right" />
 </screen>
 """
 
@@ -137,10 +138,9 @@ class PIMPmyPLiHD(ConfigListScreen, Screen):
 		self.skin_lines = []
 		Screen.__init__(self, session)
 		self.session = session
-		self.SkinTemplate = "/usr/share/enigma2/PIMP-my-PLiHD-by-talbs/skin_templates.xml"
-		self.SkinDefault = "/usr/share/enigma2/PIMP-my-PLiHD-by-talbs/skinparts/skin_default.xml"
+		self.SkinDefault = "/usr/share/enigma2/PIMP-my-PLiHD-by-talbs/default_skin.xml"
+		self.SkinDefaultTmp = self.SkinDefault + ".TMP"
 		self.SkinFinal = "/usr/share/enigma2/PIMP-my-PLiHD-by-talbs/skin.xml"
-		self.daten = "/usr/share/enigma2/PIMP-my-PLiHD-by-talbs/skinparts/"
 		list = []
 		list.append(getConfigListEntry(_("Main Color"), config.plugins.PIMPmyPLiHD.SkinColor))
 		list.append(getConfigListEntry(_("ListBox Selection Color"), config.plugins.PIMPmyPLiHD.ListBoxColor))
@@ -191,33 +191,43 @@ class PIMPmyPLiHD(ConfigListScreen, Screen):
        			
 		###########READING DATA FILES
 		try:
-			self.appendSkinFile(self.daten + "skin_templates_header.xml")
-			self.appendSkinFile(self.daten + config.plugins.PIMPmyPLiHD.InfobarWeatherWidget.value + ".xml")
-			self.appendSkinFile(self.daten + config.plugins.PIMPmyPLiHD.InfobarWeatherCityWidget.value + ".xml")
-			self.appendSkinFile(self.daten + config.plugins.PIMPmyPLiHD.PiconsChoice.value + ".xml")		
 
-			xFile = open(self.SkinTemplate, "w")
-			for xx in self.skin_lines:
+			skinSearchAndReplace = []
+			skinSearchAndReplace.append(['#00fcc000', config.plugins.PIMPmyPLiHD.SkinColor.value ])
+			if config.plugins.PIMPmyPLiHD.FontSelection.value != "nmsbd.ttf":
+				skinSearchAndReplace.append(['nmsbd.ttf', config.plugins.PIMPmyPLiHD.FontSelection.value ])
+			if config.plugins.PIMPmyPLiHD.ListBoxColor.value == "Black":
+				skinSearchAndReplace.append(['<color name="ListboxSelectedBackground" color="selectedFG"/>', '<color name="ListboxSelectedBackground" color="black"></color>' ])
+				skinSearchAndReplace.append(['<color name="ListboxSelectedForeground" color="white"/>', '<color name="ListboxSelectedForeground" color="selectedFG"></color>' ])
+			if config.plugins.PIMPmyPLiHD.InfobarWeatherWidget.value == "weather":
+				skinSearchAndReplace.append(['<panel name="MetrixNoWeatherTemplate"/>', '<panel name="MetrixWeatherTemplate"/>' ])
+			if config.plugins.PIMPmyPLiHD.InfobarWeatherCityWidget.value == "city":
+				skinSearchAndReplace.append(['<panel name="MetrixWeatherNoCityTemplate"/>', '<panel name="MetrixWeatherCityTemplate"/>' ])
+			if config.plugins.PIMPmyPLiHD.PiconsChoice.value == "shdpicon":
+				skinSearchAndReplace.append(['<panel name="InfoBarTemplate"/>', '<panel name="InfoBarTemplateSHDPicon"/>' ])				
+			
+			
+			#skin_lines = appendSkinFile2(self.SkinDefault, skinSearchAndReplace)
+
+			rsSkinLines2 = []
+
+			skFile2 = open(self.SkinDefault, "r")
+			file_lines2 = skFile2.readlines()
+			skFile2.close()
+
+			for skinLine2 in file_lines2:
+				for item in skinSearchAndReplace:
+					skinLine2 = skinLine2.replace(item[0], item[1])
+				rsSkinLines2.append(skinLine2)
+				
+			xFile = open(self.SkinDefaultTmp, "w")
+			for xx in rsSkinLines2:
 				xFile.writelines(xx)
 			xFile.close()
-	
-			q = open(self.SkinFinal,"w")
-			if config.plugins.PIMPmyPLiHD.ListBoxColor.value == "Black":
-				for line in open(self.SkinDefault):
-					line = line.replace("#0018b9ce", config.plugins.PIMPmyPLiHD.SkinColor.value )
-					line = line.replace("nmsbd.ttf", config.plugins.PIMPmyPLiHD.FontSelection.value )
-					line = line.replace("#0018b9cd", "black")
-					line = line.replace("#00fffffe", "MyColor")
-					q.write(line)
-			else:
-				for line in open(self.SkinDefault):
-					line = line.replace("#0018b9ce", config.plugins.PIMPmyPLiHD.SkinColor.value )
-					line = line.replace("nmsbd.ttf", config.plugins.PIMPmyPLiHD.FontSelection.value )
-					line = line.replace("#0018b9cd", "MyColor")
-					line = line.replace("#00fffffe", "white")
-					q.write(line)
-			q.close()
+			move(self.SkinDefaultTmp, self.SkinFinal)
 			
+			config.skin.primary_skin.setValue("PIMP-my-PLiHD-by-talbs/MyPimpedSkin.xml")
+	
 		except:
 			self.session.open(MessageBox, _("Error creating Skin!"), MessageBox.TYPE_ERROR)
 
@@ -238,7 +248,29 @@ class PIMPmyPLiHD(ConfigListScreen, Screen):
 		skFile.close()	
 		for x in file_lines:
 			self.skin_lines.append(x)
-			
+
+	def appendSkinFile2(appendFileName, skinPartSearchAndReplace):
+		"""
+		add skin file to main skin content
+
+		appendFileName:
+		 xml skin-part to add
+
+		skinPartSearchAndReplace:
+		 (optional) a list of search and replace arrays. first element, search, second for replace
+		"""
+		rsSkinLines = []
+
+		skFile = open(appendFileName, "r")
+		file_lines = skFile.readlines()
+		skFile.close()
+
+		for skinLine in file_lines:
+			for item in skinPartSearchAndReplace:
+				skinLine = skinLine.replace(item[0], item[1])
+			rsSkinLines.append(skinLine)
+
+		return rsSkinLines			
 
 	def exit(self):
 		for x in self["config"].list:
